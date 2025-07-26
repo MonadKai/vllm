d825e8be64010ab6bba3a7cb8df09572cbe27611
kai.liu <kai.liu@brgroup.com>
2025-07-26 11:17:10 -0700

    :new: Add `parrot2_audio` implementation

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -202,7 +202,7 @@ _MULTIMODAL_MODELS = {
     "Ovis": ("ovis", "Ovis"),
     "PaliGemmaForConditionalGeneration": ("paligemma", "PaliGemmaForConditionalGeneration"),  # noqa: E501
     "ParrotAudioForConditionalGeneration": ("parrot_audio", "ParrotAudioForConditionalGeneration"),  # noqa: E501
-    # "Parrot2AudioForConditionalGeneration": ("parrot2_audio", "Parrot2AudioForConditionalGeneration"),  # noqa: E501
+    "Parrot2AudioForConditionalGeneration": ("parrot2_audio", "Parrot2AudioForConditionalGeneration"),  # noqa: E501
     "Phi3VForCausalLM": ("phi3v", "Phi3VForCausalLM"),
     "PixtralForConditionalGeneration": ("pixtral", "PixtralForConditionalGeneration"),  # noqa: E501
     "QwenVLForConditionalGeneration": ("qwen_vl", "QwenVLForConditionalGeneration"),  # noqa: E501
