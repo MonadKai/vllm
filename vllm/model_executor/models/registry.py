5b0c4cf28e6bbb5dd216d09b79da4d0732f618ea
Monad Kai <onlookerliu_sjtu@163.com>
2025-12-18 15:40:02 +0800

    :new: Add `funasr_nano` model support

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -190,6 +190,7 @@ _MULTIMODAL_MODELS = {
     "Blip2ForConditionalGeneration": ("blip2", "Blip2ForConditionalGeneration"),
     "ChameleonForConditionalGeneration": ("chameleon", "ChameleonForConditionalGeneration"),  # noqa: E501
     "DeepseekVLV2ForCausalLM": ("deepseek_vl2", "DeepseekVLV2ForCausalLM"),
+    "FunasrNanoForConditionalGeneration": ("funasr_nano", "FunasrNanoForConditionalGeneration"),  # noqa: E501
     "FuyuForCausalLM": ("fuyu", "FuyuForCausalLM"),
     "Gemma3ForConditionalGeneration": ("gemma3_mm", "Gemma3ForConditionalGeneration"),  # noqa: E501
     "GLM4VForCausalLM": ("glm4v", "GLM4VForCausalLM"),
